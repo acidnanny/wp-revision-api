e120ccf10cb6165e1e4b00adcad52879ae3a70c0
Danny <danny@dannycain.com>
2018-08-10 18:13:42 +0100

    fixed dependencies (now doesn't depend on anything)

--- dpress-revisions.php
+++ dpress-revisions.php
@@ -8,6 +8,23 @@ Text Domain: dpress
 */
 
 DPressAutoload("DPress\\Revisions\\", __DIR__.'/classes/');
+spl_autoload_register(function($class) {
+	$namespace = 'DPress\\Revisions\\';
+	$path = __DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR;
+
+	if (substr($class, 0, strlen($namespace)) != $namespace)
+		return false;
+
+	$rel = substr($class, strlen($namespace));
+	$rel = strtolower(strtr($rel, array('\\' => DIRECTORY_SEPARATOR))).'.php';
+	$path = $path.$rel;
+	if (!file_exists($path))
+		return false;
+
+	require_once($path);
+	return true;
+});
+
 /**
  * @return \DPress\Revisions\RevisionsAPI
  */
